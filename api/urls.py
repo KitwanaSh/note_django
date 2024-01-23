23a85e232c83344fb963daf5f1000d2172927cf1
KitwanaSh <rodriguekitweze@gmail.com>
2024-01-23 23:35:28 +0000

    Fix: Update the code

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
     path("notes/", views.GetNotes, name="notes"),
     # path('notes/create/', views.createNote, name="create-note"),
     # path('notes/<str:pk>/update/', views.UpdateNote, name="update-note"),
--- api/views.py
+++ api/views.py
@@ -4,44 +4,6 @@ from .models import Note
 from .serializers import NoteSerializer
 from .utils import getNoteList, createNote, getSingleNote, UpdateNote, DeleteNote
 
-
-@api_view(['GET'])
-def getRoutes(request):
-
-    routes = [
-        {
-            'Endpoint': '/notes/',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns an array of notes'
-        },
-        {
-            'Endpoint': '/notes/id',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns a single note object'
-        },
-        {
-            'Endpoint': '/notes/create/',
-            'method': 'POST',
-            'body': {'body': ""},
-            'description': 'Creates new note with data sent in post request'
-        },
-        {
-            'Endpoint': '/notes/id/update/',
-            'method': 'PUT',
-            'body': {'body': ""},
-            'description': 'Creates an existing note with data sent in post request'
-        },
-        {
-            'Endpoint': '/notes/id/delete/',
-            'method': 'DELETE',
-            'body': None,
-            'description': 'Deletes and exiting note'
-        },
-    ]
-    return Response(routes)
-
 # List of how the url should look like
 
 # /notes GET
